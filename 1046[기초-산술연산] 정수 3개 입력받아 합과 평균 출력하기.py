d16baafa5c301839d12a98b1a861c2653de26fd7
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-08-30 01:13:57 +0900

    46, 69 71 문제 추가 (20.08.30)
    46, 69 71 문제 품

---
+++ 1046[기초-산술연산] 정수 3개 입력받아 합과 평균 출력하기.py
@@ -0,0 +1,4 @@
+a,b,c = map(int, input().split())
+sum = a+b+c
+print(sum)
+print("{:.1f}".format(float(sum)/3))
---
+++ 1071[기초-반복실행구조] 0 입력될 때까지 무한 출력하기1.py
@@ -0,0 +1,7 @@
+num = list(map(int, input().split()))
+i=0
+while True:
+    if num[i] == 0:
+        break
+    print(num[i])
+    i+=1
